a1059b41ee36b61b689f16fe2252a1c130849062
Maxime Franco <maxime.franco@acsone.eu>
2022-03-05 10:19:18 +0100

    First commit

---
+++ ai/bot.py
@@ -0,0 +1,39 @@
+from sdk import *
+import sys
+from time import time
+
+# Reading my team id from the command line args
+team_id = int(sys.argv[1])
+
+def game_loop(game_state: GameState, game_time: int, log: Logger) -> PlayerOrder:
+    now = time()
+    log.info(f"Received the game state. My team is {team_id}")
+
+    my_cars = [car for car in game_state.cars if car.team_id == team_id]
+    if game_state.is_started:
+        # The game has started. Deciding what to do with our two ships
+        def order_for_car(car: Car) -> PlayerOrder:
+            # Here we decided to apply the same rule for our two ships
+            if not car.boost_used:
+                # The boost is still free to use, let's use it!
+                return UseBoost(car.id, team_id)
+            # The boost has already been used. We decide to move towards the next checkpoint (planet)
+            next_checkpoint = car.next_checkpoint(game_state)
+            # The following is morally equivalent to
+            # ApplyForce(car.id, team_id, [argument of next_checkpoint.pos - car.pos], 100)
+            # (tip: that argument can be computed with atan2)
+            return ForceTowards(car.id, team_id, next_checkpoint.pos, 100)
+        log.info("I'm creating my order")
+        order = OrderForEachCar(order_for_car(my_cars[0]), order_for_car(my_cars[1]))
+
+    else:
+        # Game has not yet started. It is the only place where we can (and must!) set the masses for the ships.
+        order = SetCarMasses(team_id, my_cars[0].id, my_cars[1].id, 5, 15)
+
+    time_taken = time() - now
+    # Logging how much time it took.
+    log.info(f"It took {time_taken * 1000000} micro seconds to complete.")
+    return order
+
+# Starting the game loop, you can leave this line as is.
+Runner(team_id, game_loop).run()
---
+++ ai/sdk.py
@@ -0,0 +1,301 @@
+import json
+from typing import Callable, Dict, List, Optional, Tuple
+
+def complex_from_json(info: Dict) -> complex:
+    return info["re"] + 1j * info["im"]
+
+def complex_to_json(z: complex) -> Dict:
+    return {"re": z.real, "im": z.imag}
+
+class Circle:
+
+    @staticmethod
+    def from_json(info: Dict):
+        return Circle(info["radius"])
+
+    def __init__(self, radius: float):
+        self.radius = radius
+
+class CheckpointInfo:
+
+    @staticmethod
+    def from_json(info: Dict):
+        return CheckpointInfo(info["checkpointIndex"], info["time"])
+
+    def __init__(self, index: int, time: int):
+        self.index = index
+        self.time = time
+
+class Car:
+
+    @staticmethod
+    def from_json(info: Dict):
+        return Car(
+            info["id"],
+            info["time"],
+            info["teamId"],
+            complex_from_json(info["pos"]),
+            complex_from_json(info["speed"]),
+            info["rotation"],
+            info["mass"],
+            Circle.from_json(info["shape"]),
+            [CheckpointInfo.from_json(checkpoint_info_info) for checkpoint_info_info in info["passedCheckpoints"]],
+            info["boostUsed"]
+        )
+
+    def __init__(
+            self,
+            car_id: int, time: int, team_id: int, pos: complex,
+            speed: complex, rotation: float, mass: float,
+            shape: Circle,
+            passed_checkpoints: List[CheckpointInfo],
+            boost_used: bool
+    ):
+        self.id = car_id
+        self.time = time
+        self.team_id = team_id
+        self.pos = pos
+        self.speed = speed
+        self.rotation = rotation
+        self.mass = mass
+        self.shape = shape
+        self.passed_checkpoints: List["CheckpointInfo"] = passed_checkpoints
+        self.boost_used = boost_used
+
+    def next_checkpoint_index(self, number_of_checkpoints: int) -> int:
+        return (self.passed_checkpoints[0].index + 1) % number_of_checkpoints
+
+    def next_next_checkpoint_index(self, number_of_checkpoints: int) -> int:
+        return (self.passed_checkpoints[0].index + 2) % number_of_checkpoints
+
+    def next_checkpoint(self, game_state: "GameState") -> "Checkpoint":
+        index = self.next_checkpoint_index(game_state.number_of_checkpoints)
+        return next(checkpoint for checkpoint in game_state.checkpoints if checkpoint.checkpoint_index == index)
+
+    def next_next_checkpoint(self, game_state: "GameState") -> "Checkpoint":
+        index = self.next_next_checkpoint_index(game_state.number_of_checkpoints)
+        return next(checkpoint for checkpoint in game_state.checkpoints if checkpoint.checkpoint_index == index)
+
+class Checkpoint:
+
+    @staticmethod
+    def from_json(info: Dict):
+        return Checkpoint(info["id"], info["time"], complex_from_json(info["pos"]), Circle.from_json(info["shape"]), info["checkpointIndex"])
+
+    def __init__(self, checkpoint_id: int, time: int, pos: complex, shape: Circle, index: int):
+        self.id = checkpoint_id
+        self.time = time
+        self.pos = pos
+        self.shape = shape
+        self.checkpoint_index = index
+
+class GameState:
+
+    @staticmethod
+    def from_json(info: Dict):
+        return GameState(info["time"], info.get("maybeStartedTime"),
+                         info.get("maybeEndedTime"),
+                         [Car.from_json(car_info) for car_info in info.get("cars", [])],
+                         [Checkpoint.from_json(checkpoint_info) for checkpoint_info in info.get("checkpoints", [])],
+                         info.get("crashedTeams"), info["totalNumberOfLaps"])
+
+    def __init__(
+            self,
+            time: int,
+            start_time: Optional[int],
+            end_time: Optional[int],
+            cars: List[Car],
+            checkpoints: List[Checkpoint],
+            crashed_teams: List[Tuple[int, bool]],
+            total_number_of_laps: int
+    ):
+        self.time = time
+        self.start_time = start_time
+        self.end_time = end_time
+        self.cars = cars
+        self.checkpoints = checkpoints
+        self.crashed_teams = crashed_teams
+        self.total_number_of_laps = total_number_of_laps
+
+    @property
+    def is_started(self) -> bool:
+        return self.start_time is not None
+
+    @property
+    def number_of_checkpoints(self) -> int:
+        return len(self.checkpoints)
+
+class PlayerOrder:
+
+    def __init__(self, type_name: str):
+        self.type_name = "com.b12.gamerunning.PlayerOrder$" + type_name
+
+    def to_json(self):
+        raise NotImplementedError()
+
+    @property
+    def full_json(self):
+        return {
+            "type": self.type_name,
+            "info": self.to_json()
+        }
+
+class LoggingOrder(PlayerOrder):
+
+    def __init__(self, message: str, level: str, order: PlayerOrder):
+        super().__init__("LoggingOrder")
+        self.message = message
+        self.level = level
+        self.order = order
+
+    def to_json(self):
+        return {
+            "message": self.message,
+            "logLevel": self.level,
+            "order": self.order.full_json
+        }
+
+class Pass(PlayerOrder):
+
+    def __init__(self, team_id: int):
+        super().__init__("Pass")
+        self.team_id = team_id
+
+    def to_json(self):
+        return {
+            "teamId": self.team_id
+        }
+
+class ApplyForce(PlayerOrder):
+
+    def __init__(self, car_id: int, team_id: int, angle: float, power: int):
+        super().__init__("ApplyForce")
+        self.car_id = car_id
+        self.team_id = team_id
+        self.angle = angle
+        self.power = power
+        if not type(self.power) == int:
+            raise ValueError(f"Power must be integer, got {self.power} ({type(self.power)})")
+
+    def to_json(self):
+        return {
+            "carId": self.car_id,
+            "teamId": self.team_id,
+            "forceAngle": self.angle,
+            "power": self.power
+        }
+
+class ForceTowards(PlayerOrder):
+
+    def __init__(self, car_id: int, team_id: int, target_position: complex, power: int):
+        super().__init__("ForceTowards")
+        self.car_id = car_id
+        self.team_id = team_id
+        self.target_position = target_position
+        self.power = power
+        if not type(self.power) == int:
+            raise ValueError(f"Power must be integer, got {self.power} ({type(self.power)})")
+
+    def to_json(self):
+        return {
+            "carId": self.car_id,
+            "teamId": self.team_id,
+            "targetPosition": complex_to_json(self.target_position),
+            "power": self.power
+        }
+
+class UseBoost(PlayerOrder):
+
+    def __init__(self, car_id: int, team_id: int):
+        super().__init__("UseBoost")
+        self.car_id = car_id
+        self.team_id = team_id
+
+    def to_json(self):
+        return {
+            "carId": self.car_id,
+            "teamId": self.team_id
+        }
+
+class Crash(PlayerOrder):
+    def __init__(self, team_id: int, reason: str):
+        super().__init__("Crash")
+        self.team_id = team_id
+        self.reason = reason
+
+    def to_json(self):
+        return {
+            "teamId": self.team_id,
+            "reason": self.reason
+        }
+
+class OrderForEachCar(PlayerOrder):
+
+    def __init__(self, order_1: PlayerOrder, order_2: PlayerOrder):
+        super().__init__("OrderForEachCar")
+        self.order_1 = order_1
+        self.order_2 = order_2
+
+    def to_json(self):
+        return {
+            "car1Order": self.order_1.full_json,
+            "car2Order": self.order_2.full_json
+        }
+
+class SetCarMasses(PlayerOrder):
+
+    def __init__(self, team_id: int, car1_id: int, car2_id: int, car1_mass: int, car2_mass: int):
+        self.team_id = team_id
+        self.car1_id = car1_id
+        self.car2_id = car2_id
+        self.car1_mass = car1_mass
+        self.car2_mass = car2_mass
+        super().__init__("SetCarMasses")
+
+    def to_json(self):
+        return {
+            "teamId": self.team_id,
+            "car1Id": self.car1_id,
+            "car1Mass": self.car1_mass,
+            "car2Id": self.car2_id,
+            "car2Mass": self.car2_mass
+        }
+
+class Logger:
+
+    def __init__(self):
+        self.lines = []
+
+    def info(self, message: str):
+        self.lines.append({"message": message, "level": "Info"})
+
+    def error(self, message: str):
+        self.lines.append({"message": message, "level": "Error"})
+
+    def order(self, order: PlayerOrder) -> LoggingOrder:
+        final_order = order
+        for message_info in self.lines:
+            final_order = LoggingOrder(message_info["message"], message_info["level"], final_order)
+        return final_order
+
+def _send_order(log: Logger, order: PlayerOrder):
+    print(json.dumps(log.order(order).full_json))
+
+
+class Runner:
+
+    def __init__(self, team_id: int, game_loop: Callable[[GameState, int, Logger], PlayerOrder]):
+        self.team_id = team_id
+
+        self.game_loop = game_loop
+
+    def run(self):
+        while True:
+            raw_input = json.loads(input())
+            log = Logger()
+            game_state = GameState.from_json(raw_input["gameState"])
+            time = raw_input["time"]
+
+            order = self.game_loop(game_state, time, log)
+
+            _send_order(log, order)
